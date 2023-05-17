028fd4409b5f0c5d61446eb3b38ea131df0e9d35
Muhammad Aqib Shah <109861813+Muh-Aqib-Shah@users.noreply.github.com>
2023-05-17 14:42:36 +0000

    CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { QuestionCard } from './QuestionCard';
 import { Difficulty, QuestionsAPI,Question } from './QuestionAPI';
@@ -7,29 +7,28 @@ import { Difficulty, QuestionsAPI,Question } from './QuestionAPI';
 function App() {
   
   const TOTAL_QUESTIONS = 10
-  const LISTED_ANSWERS = document.getElementsByClassName("basic")
+  const OPTIONS = document.getElementsByClassName("basic")  
   
   let [loading,setLoading] = useState(true)
   let [quizStarted,setQuizStarted] = useState(false)
   let [questions,setQuestions] = useState<Question[]>([])
   let [questionNum,setQuestionNum] = useState(0)
   let [score,setScore] = useState(0)
-  let [useranswer,setUseranswer] = useState(false)
-  let [disabled,setDisabled] = useState(true)
+  let [disabled,setDisabled] = useState(false)
 
   const startQUIZ = async() => {
     setLoading(true);
-    LISTED_ANSWERS.map(ans=>
-      console.log("ANS:",ans);
-      )
-    console.log("ITEMS",LISTED_ANSWERS);
-    
     const data = await QuestionsAPI(TOTAL_QUESTIONS,Difficulty.EASY)
     setQuestions(data)
     setLoading(false);
     setQuizStarted(true)
+    setQuestionNum(0)
+    setScore(0)
 }
  const nextQuestion = () => {
+  
+  setOptions("showBasic")
+  setDisabled(false)
   const q_number = questionNum + 1
   if(q_number >= TOTAL_QUESTIONS)
     setQuizStarted(false)
@@ -37,7 +36,25 @@ function App() {
    setQuestionNum(q_number)  
  }
 
+  const setOptions = (choice: string) => {
+   
+    switch(choice){
+    case "showAnswer":
+      Array.from(OPTIONS).forEach(plant=>{
+        if(plant.innerHTML === questions[questionNum].correct_answer)
+          plant.id = "correct"
+      })
+      break
+    case "showBasic":
+      Array.from(OPTIONS).forEach((plant,index)=>{
+      plant.id =  `answr-btn${index}`
+      })
+      break
+    }
+  }
+
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+  setDisabled(true)
    const corr = e.currentTarget.value
    console.log("VAL:",corr);
    console.log('ID:',e.currentTarget.id);
@@ -47,6 +64,8 @@ function App() {
      e.currentTarget.id = "correct"}
   else
    e.currentTarget.id = "wrong"
+   setOptions("showAnswer")
+
  }
 
   return (
@@ -62,7 +81,7 @@ function App() {
       questions = {questions[questionNum].question}
       answers={questions[questionNum].answers}
       callback={checkAnswer}
-      Disabled={!disabled}
+      Disabled={disabled}
       TotalQuestions={TOTAL_QUESTIONS} />:null}
      {quizStarted && disabled?
      <button className='next-btn' onClick={()=>nextQuestion()}>Next</button> : null
--- src/QuestionCard.tsx
+++ src/QuestionCard.tsx
@@ -10,16 +10,16 @@ type DataTypes = {
 }
 
 export const QuestionCard: React.FC<DataTypes> = ({questions,answers,callback,questionNum,Disabled,TotalQuestions}) => {
-
+  
     return (
       <div className='quest-container'>
         <div className='quest-box'>
           <div className='quest-status'>Question: {questionNum}/{TotalQuestions}</div>
           <div className='quest' dangerouslySetInnerHTML={{__html:questions}} ></div>
-          <div className='answer-box'>
+          <div id="options-box" className='answer-box' >
             {
               answers.map((answer,index) =>(
-                <button id= {`answr-btn${index}`} className="basic" value={answer} onClick={callback} disabled={Disabled} dangerouslySetInnerHTML={{__html:answer}} />
+                <button key={index} id= {`answr-btn${index}`} className="basic" value={answer} onClick={callback} disabled={Disabled} dangerouslySetInnerHTML={{__html:answer}} />
               ))
             }
           </div>
